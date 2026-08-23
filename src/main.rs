275f215fea02cfd26e8c7b27bbf94b646b37f308
Utsho MH <utshomh.dev@gmail.com>
2026-08-23 23:37:40 +0600

    add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,9 @@ async fn main() -> Result<()> {
 
     info!("started http server: http://{addr}");
 
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
 
     Ok(())
 }
@@ -37,6 +39,32 @@ fn init_logging() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
 
 async fn root() -> &'static str {
